d00023d5a938afe33a1f0cd94b13092addf2e200
qkrtkdgus010208 <psh010208@naver.com>
2025-07-20 19:44:15 +0900

    유니티 뱀서라이크 #03
    새로운 인풋시스템 적용하기

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    public Vector2 inputVec;
+    public float speed;
+
+    Rigidbody2D rigid;
+
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 nextVec = inputVec * speed * Time.fixedDeltaTime;
+        rigid.MovePosition(rigid.position + nextVec);
+    }
+
+    void OnMove(InputValue value)
+    {
+        inputVec = value.Get<Vector2>();
+    }
+}
