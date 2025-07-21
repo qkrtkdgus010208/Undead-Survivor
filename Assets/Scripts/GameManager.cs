22aa7a6615ddae49cc576c643e5d97be06b38d57
qkrtkdgus010208 <psh010208@naver.com>
2025-07-21 23:45:25 +0900

    유니티 뱀서라이크 #08
    소환 레벨 적용하기

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,11 +3,25 @@
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
+
+    public float gameTime;
+    public float maxGameTime = 2 * 10f;
+
     public PoolManager pool;
     public Player player;
 
     private void Awake()
     {
         instance = this;
+    }
+
+    private void Update()
+    {
+        gameTime += Time.deltaTime;
+
+        if (gameTime > maxGameTime)
+        {
+            gameTime = maxGameTime;
+        }
     }
 }
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -3,14 +3,17 @@
 public class Spawner : MonoBehaviour
 {
     public Transform[] spawnPoint;
+    public SpawnData[] spawnData;
 
+    int level;
     float timer;
 
     private void Update()
     {
         timer += Time.deltaTime;
+        level = Mathf.Min(Mathf.FloorToInt(GameManager.instance.gameTime / 10f), spawnData.Length - 1);
 
-        if (timer > 1f)
+        if (timer > spawnData[level].spawneTime)
         {
             timer = 0f;
             Spawn();
@@ -19,7 +22,17 @@ public class Spawner : MonoBehaviour
 
     private void Spawn()
     {
-        GameObject enemy = GameManager.instance.pool.Get(Random.Range(0, 5));
+        GameObject enemy = GameManager.instance.pool.Get(0);
         enemy.transform.position = spawnPoint[Random.Range(0, spawnPoint.Length)].position;
+        enemy.GetComponent<Enemy>().Init(spawnData[level]);
     }
+}
+
+[System.Serializable]
+public class SpawnData
+{
+    public float spawneTime;
+    public int spriteType;
+    public int health;
+    public float speed;
 }
