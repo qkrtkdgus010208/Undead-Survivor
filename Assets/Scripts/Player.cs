1fed0b34841e10efbb37e45b8176eac418d5f97b
qkrtkdgus010208 <psh010208@naver.com>
2025-07-25 21:10:24 +0900

    유니티 뱀서라이크 #14
    플레이어 무기 장착 표현하기

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,6 +6,7 @@ public class Player : MonoBehaviour
     public Vector2 inputVec;
     public float speed;
     public Scanner scanner;
+    public Hand[] hands;
 
     Rigidbody2D rigid;
     SpriteRenderer spriter;
@@ -17,6 +18,7 @@ public class Player : MonoBehaviour
         spriter = GetComponent<SpriteRenderer>();
         anim = GetComponent<Animator>();
         scanner = GetComponent<Scanner>();
+        hands = GetComponentsInChildren<Hand>(true);
     }
 
     private void FixedUpdate()
